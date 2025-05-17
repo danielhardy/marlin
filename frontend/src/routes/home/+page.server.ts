28ae2e0a0dc664bbafe61fa487bff2e142319194
Daniel Hardy <daniel@danielhardy.com>
2025-05-17 10:15:54 -0600

    Revised naming for account_token to plaid_item_id which is more accurate

--- frontend/src/routes/home/+page.server.ts
+++ frontend/src/routes/home/+page.server.ts
@@ -14,7 +14,7 @@ export const load: PageServerLoad = async ({ locals }) => {
 	console.log('business_id in /home/+page.server.ts', business_id);
 
 	// Fetch the access_token from plaid_items for the user's business
-	let access_tokens = null;
+	let plaid_item_ids = null;
 	if (business_id) {
 		const { data: fetched_access_tokens, error } = await supabase
 			.from('plaid_items')
@@ -25,9 +25,9 @@ export const load: PageServerLoad = async ({ locals }) => {
 			console.error('Error fetching access_token:', error);
 		}
 		if (!error && fetched_access_tokens) {
-			access_tokens = fetched_access_tokens;
+			plaid_item_ids = fetched_access_tokens;
 		}
-		console.log('access_tokens', access_tokens);
+		console.log('plaid_item_ids', plaid_item_ids);
 	}
 
 	// Return the session (and optionally the business data if needed)
@@ -36,6 +36,6 @@ export const load: PageServerLoad = async ({ locals }) => {
 		user: session?.user ?? null,
 		business_id: business_id,
 		cookies: [],
-		access_tokens
+		plaid_item_ids
 	};
 };
